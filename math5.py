3e2292b3c44700e6bc16920332e92dddac3ff12b
TereRoxy <tere.roxy.rt@gmail.com>
2019-04-18 14:04:00 +0300

    defineste f(x)

---
+++ math5.py
@@ -0,0 +1,12 @@
+import math
+import numpy as np
+
+a = float(input("Introdu coeficientul lui x: "))
+b = float(input("Introdu termenul liber: "))
+
+def f(x):
+    return a*x+b
+
+print(f(1))
+
+input ("Apasa pe orice tasta ca sa iesi")
