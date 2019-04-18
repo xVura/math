207e120dbee08371340d29c90e750e37117bd839
xVura <rares.vura@yahoo.com>
2019-04-18 12:47:36 +0200

    Vura's commit

--- math3.py
+++ math3.py
@@ -1,16 +1,32 @@
+
+# SCRIPT DE PROBA
 import numpy as np
 import matplotlib.pyplot as plt
-# SCRIPT DE PROBA
+import math
+
 
 fig = plt.figure()
 
-x = np.linspace(1, 4)
-y = np.linspace(2, 5)
+
+
+x = float(input("Introdu numarul x: "))
+y = float(input("Introdu numarul y: "))
+
+if x > 0 and y > 0 :
+    #x1 = np.linspace(x, math.inf)
+    #y1 = np.linspace(y, math.inf)
+    x1 = np.linspace(x, 0)
+    y1 = np.linspace(y, 0)
+if x < 0 and y < 0 :
+    #x1 = np.linspace(-math.inf, x)
+    #y1 = np.linspace(-math.inf, y)
+    x1 = np.linspace(0, x)
+    y1 = np.linspace(0, y)
 
 
 ax = plt.gca()
 
-ax.plot(x, y)
+ax.plot(x1, y1)
 ax.grid(True)
 ax.spines['left'].set_position('zero')
 ax.spines['right'].set_color('none')
