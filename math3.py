6923d9dbd36ca1456d38be8726f51b87ca1d4105
xVura <rares.vura@yahoo.com>
2019-04-18 15:11:49 +0200

    vurica

--- math3.py
+++ math3.py
@@ -45,7 +45,18 @@ ax.spines['left'].set_position('zero')
 ax.spines['right'].set_color('none')
 ax.spines['bottom'].set_position('zero')
 ax.spines['top'].set_color('none')
-plt.xlim(-np.pi,np.pi)
+def extended(ax, x, y, **args):
+
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+
+    x_ext = np.linspace(xlim[0], xlim[1], 100)
+    p = np.polyfit(x, y , deg=1)
+    y_ext = np.poly1d(p)(x_ext)
+    ax.plot(x_ext, y_ext, **args)
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+    return ax
 
 plt.savefig("CenterOriginMatplotlib01.png")
 plt.show()
