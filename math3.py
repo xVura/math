8107da0efca7ed701acbd6d0359af76f9f5cc06d
xVura <rares.vura@yahoo.com>
2019-04-18 14:03:26 +0200

    Vura's commit

--- math3.py
+++ math3.py
@@ -45,7 +45,6 @@ ax.spines['left'].set_position('zero')
 ax.spines['right'].set_color('none')
 ax.spines['bottom'].set_position('zero')
 ax.spines['top'].set_color('none')
-
 plt.xlim(-np.pi,np.pi)
 
 plt.savefig("CenterOriginMatplotlib01.png")
