cdbb6370e23536b6228af785860645895c6be032
TereRoxy <tere.roxy.rt@gmail.com>
2019-04-22 21:37:01 +0300

    kk

--- math4.py
+++ math4.py
@@ -14,13 +14,20 @@ b1 = float(input("Introdu termenul liber din f(x): "))
 a2 = float(input("Introdu coeficientul lui x din g(x): "))
 b2 = float(input("Introdu termenul liber din g(x): "))
 
-# se definesc si determina variabilele
-x = (b2-b1)/(a1-a2)
-y = a1*x + b1
+# se definesc variabilele
+x1 = (b2-b1)/(a1-a2)
 
-# afiseaza rezultatul
-print ("Graficele functiilor se intersecteaza in punctul de coordonate: ", end= "\n")
-print ("(", x, ", ", y, ")", end='\n')
+def f(x):
+    return a1*x + b1
+def g(x):
+    return a2*x + b2
+
+if a1 == a2:
+    print ("Graficele celor doua functii reprezinta drepte paralele")
+else:
+    # afiseaza rezultatul
+    print ("Graficele functiilor se intersecteaza in punctul de coordonate: ", end= "\n")
+    print ("(", x1, ", ", f(x1), ")", end='\n')
 
 # pastreaza programul deschis in consola
 input ("Apasa pe orice tasta ca sa iesi: ")
--- math5 test.py
+++ math5 test.py
@@ -13,26 +13,12 @@ m = -b/a
 def f(x):
   return a*x+b
 
-# defineste variabilele necesare pt grafic
-# defineste variabilele folosite pt |b|<100*
-x1 = 100
-x2 = f(100)
-y1 = -100
-y2 = f(-100)
-
 # transforma intervale in "puncte"
 A = np.linspace (m, 0)
 B = np.linspace (0, b)
-C = np.linspace (x1, y1)
-D = np.linspace (x2, y2)
-<<<<<<< HEAD
+C = np.linspace (100, -100)
+D = np.linspace (f(100), f(-100))
 
-=======
-E = np.linspace (z1, t1)
-F = np.linspace (z2, t2)
-G = np.linspace (g1, h1)
-H = np.linspace (g2, h2)
->>>>>>> ea05e04b304ce1a995f82fe5b36dbe56f05281da
 # afiseaza coordonatele punctelor determinate de intersectiile cu sistemul ortogonal de axe
 print ("Graficul functiei f(x) intersecteaza axa Ox in punctul: ")
 print ("(", end="")
@@ -49,21 +35,9 @@ print(")")
 # traseaza graficul
 ax = plt.gca()
 
-<<<<<<< HEAD
 ax.plot(C, D)
 ax.plot(A, B)
 
-=======
-if abs(b) < 100:
-    ax.plot(C, D)
-
-elif abs(b) < 10000:
-    ax.plot(E, F)
-
-else:
-    ax.plot(G, H)
-ax.plot(A,B)
->>>>>>> ea05e04b304ce1a995f82fe5b36dbe56f05281da
 ax.grid(True)
 ax.spines['left'].set_position('zero')
 ax.spines['right'].set_color('none')
